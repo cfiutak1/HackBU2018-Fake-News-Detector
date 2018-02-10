d46b4adecd145c3fc0c4750d698679113ee166dd
cfiutak1 <cfiutak1@binghamton.edu>
2018-02-10 15:47:42 -0500

    Added text algorithm, needs to be tested and we need to determine which factors to use

---
+++ text_algorithm.py
@@ -0,0 +1,30 @@
+from textstat.textstat import textstat
+import language_check
+
+
+def get_text_characteristics(article_contents: str) -> dict:
+    """
+    Takes an article's contents and analyzes its complexity using numerous reading scores and methods. Also calculates
+    other factors such as the number of typos.
+
+    @param article_contents, a string which contains the contents of an article
+    @return language_analysis_dict, a dictionary which contains
+    """
+    tool = language_check.LanguageTool('en-US')
+    language_analysis_dict = {
+        "flesch_reading": textstat.flesch_reading_ease(article_contents),
+        "smog": textstat.smog_index(article_contents),
+        "flesch_kincaid": textstat.flesch_kincaid_grade(article_contents),
+        "coleman_liau": textstat.coleman_liau_index(article_contents),
+        "ari": textstat.automated_readability_index(article_contents),
+        "dale_chall": textstat.dale_chall_readability_score(article_contents),
+        "difficult_words": textstat.difficult_words(article_contents),
+        "linsear_write": textstat.linsear_write_formula(article_contents),
+        "gunning_fog": textstat.gunning_fog(article_contents),
+        "text_standard": textstat.text_standard(article_contents),
+        "lexicon_count": textstat.lexicon_count(article_contents),
+        "language_check_typos": len(tool.check(article_contents)),
+        "typos_to_words": len(tool.check(article_contents)) / textstat.lexicon_count(article_contents),
+    }
+
+    return language_analysis_dict
