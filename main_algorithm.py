1fc8395f120d0d12e1e043135cff155ba347df3d
hbrown10 <haydenbrown@comcast.net>
2018-02-11 02:18:36 -0500

    added populate_json to main

--- main_algorithm.py
+++ main_algorithm.py
@@ -2,6 +2,7 @@ import google_search
 import page_scraper
 import text_algorithm
 import whois_algorithm
+import json
 
 
 def get_results(url: str) -> dict:
@@ -26,3 +27,21 @@ def get_results(url: str) -> dict:
         "percent_difficult_words": text_dict["percent_difficult_words"],
         "google_search_similarity": google_var,
     }
+
+
+def populate_json(jsonfile, jsontarget):
+    
+    for line in jsonfile:
+        articlebody = page_scraper.get_article_content(jsontarget[line]["URL"])
+        articledata = text_algorithm.get_text_features(articlebody)
+
+        jsontarget[line]["flesch_reading"] = articledata["flesch_reading"]
+        jsontarget[line]["flesch_kincaid"] = articledata["flesch_kincaid"]
+        jsontarget[line]["coleman_liau"] = articledata["coleman_liau"]
+        jsontarget[line]["typo_pct"] = articledata["typos_to_words"]
+        jsontarget[line]["diff_word_pct"] = articledata["percent_difficult_words"]
+        jsontarget[line]["google_search_hits"] = google_search.get_google_search_feature(articlebody)
+        jsontarget[line]["whois_location"] = whois_algorithm.get_whois_features(jsonfile[line]["URL"])["location"]
+        jsontarget[line]["whois_age"] = whois_algorithm.get_whois_features(jsonfile[line]["URL"])["age"]
+        jsontarget[line]["fake"] = jsonfile[line]["fake"]
+
